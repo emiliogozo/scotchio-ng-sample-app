e40d80b82c46b2e59be6e5482f64e6679458573d
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-04-22 20:24:27 +0800

    feat: Show active user

--- app/app.component.ts
+++ app/app.component.ts
@@ -19,16 +19,20 @@ import { Component } from '@angular/core';
             <ul class="list-group users-list">
               <li class="list-group-item"
                 *ngFor="let user of users"
-                (click)="selectUser(user)">
+                (click)="selectUser(user)"
+                [class.active]="user === activeUser">
                 {{user.name}} ({{user.username}})
               </li>
             </ul>
           </div>
         </div>
         <div class="col-sm-8">
-          <div class="jumbotron">
-            <h1>Welcome to Our App!</h1>
-            <p>{{message}}</p>
+          <div class="jumbotron" *ngIf="activeUser">
+            <h2>{{activeUser.name}} <small>{{activeUser.username}}</small></h2>
+          </div>
+          <div class="jumbotron" *ngIf="!activeUser">
+            <span class="glyphicon glyphicon-hand-left"></span>
+            <h2>Choose a User</h2>
           </div>
         </div>
       </div>
@@ -39,7 +43,12 @@ import { Component } from '@angular/core';
     </footer>
   `,
   styles: [`
-    .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
+    .users-list li {
+      cursor: pointer;
+    }
+    .jumbotron .glyphicon {
+      font-size: 80px;
+    }
   `]
 })
 export class AppComponent {
